813b5104683341c9f24eccdb51d68465fb7565c0
Abdulrahman Almahaini <abd.mah.useinsider@gmail.com>
2025-01-23 04:49:01 +0300

    SD-114867 | upgrade the facebook request versions. (#1)

--- src/FacebookAds/ApiConfig.php
+++ src/FacebookAds/ApiConfig.php
@@ -9,7 +9,7 @@
 
 namespace FacebookAds;
 class ApiConfig {
-  const APIVersion = '19.0';
-  const SDKVersion = '19.0.0';
+  const APIVersion = '21.0';
+  const SDKVersion = '21.0.0';
   const TYPE_CHECKER_STRICT_MODE = false;
 }
