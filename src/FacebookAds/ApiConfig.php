5bdd7faf6864db94e61499ba71a3c363d95d3bdf
abd-mah-useinsider <abd.mah.useinsider@gmail.com>
2026-01-09 19:08:54 +0300

    SD-133410 | Facebook API upgrade

--- src/FacebookAds/ApiConfig.php
+++ src/FacebookAds/ApiConfig.php
@@ -9,7 +9,7 @@
 
 namespace FacebookAds;
 class ApiConfig {
-  const APIVersion = '21.0';
-  const SDKVersion = '21.0.0';
+  const APIVersion = '24.0';
+  const SDKVersion = '24.0.0';
   const TYPE_CHECKER_STRICT_MODE = false;
 }
